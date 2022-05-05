a5f864e3e67f0b57afeb3cde5b5086666666156c
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-05-05 18:11:19 +0300

    added solution with promise

--- 01.XHR/scripts/app.js
+++ 01.XHR/scripts/app.js
@@ -1,13 +1,33 @@
 function loadRepos() {
+   const resEl = document.getElementById('res');
    const url = 'https://api.github.com/users/testnakov/repos';
 
-   const xmlHttpRequest = new XMLHttpRequest();
-   xmlHttpRequest.addEventListener('readystatechange', state => {
-      if (xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200) {
-         console.log(xmlHttpRequest.responseText);
-      }
-   });
-   
-   xmlHttpRequest.open('GET', url);
-   xmlHttpRequest.send();
+   // const xmlHttpRequest = new XMLHttpRequest();
+   // xmlHttpRequest.addEventListener('readystatechange', state => {
+   //    if (xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200) {
+   //       resEl.innerHTML = xmlHttpRequest.responseText;
+   //    }
+   // });
+
+   // xmlHttpRequest.open('GET', url);
+   // xmlHttpRequest.send();
+
+
+   fetch(url)
+      .then(res => {
+         if (res.status === 200) {
+            return res.json();
+         } else if (res.status === 401) {
+            console.warn('UNAUTHORIZED');
+         } else if (res.status === 500) {
+            console.log('Server Error');
+         }
+      })
+      .then(data => {
+         if (!data) {
+            return;
+         }
+         console.log(data);
+         resEl.innerHTML = JSON.stringify(data);
+      });
 }
