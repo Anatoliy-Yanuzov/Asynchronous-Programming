7effc633f810c0f922b59639024ac5125715d20e
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-05-05 17:52:32 +0300

    init

---
+++ 01.XHR/scripts/app.js
@@ -0,0 +1,12 @@
+function loadRepos() {
+   const url = 'https://api.github.com/users/testnakov/repos';
+
+   const xmlHttpRequest = new XMLHttpRequest();
+   xmlHttpRequest.addEventListener('readystatechange', state => {
+      if (xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200) {
+         console.log(xmlHttpRequest.responseText);
+      }
+   });
+   xmlHttpRequest.open('GET', url);
+   xmlHttpRequest.send();
+}
