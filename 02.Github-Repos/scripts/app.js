aa6294c47e8bdffd08813d6943d24325ca07ed79
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-05-06 18:05:04 +0300

    added solution

--- 02.Github-Repos/scripts/app.js
+++ 02.Github-Repos/scripts/app.js
@@ -1,3 +1,21 @@
+const reposEl = document.querySelector('#repos');
+const usernameEl = document.querySelector('#username');
+
 function loadRepos() {
-	console.log("TODO...");
+	reposEl.innerHTML = '<h1 id="loading">Loading...</h1>';
+	const username = usernameEl.value;
+	const url = `https://api.github.com/users/${username}/repos`;
+	fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			document.getElementById('loading').remove();
+			data.forEach(elem => {
+				let li = document.createElement('li');
+				let a = document.createElement('a');
+				a.href = elem.html_url;
+				a.innerHTML = elem.full_name;
+				li.appendChild(a);
+				reposEl.appendChild(li);
+			});
+		})
 }
