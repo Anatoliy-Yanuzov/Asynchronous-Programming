1a7baecafd6a57286f2a88273ce9b06e1ecdb434
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-05-07 00:53:47 +0300

    added response when status is 404

--- 02.Github-Repos/scripts/app.js
+++ 02.Github-Repos/scripts/app.js
@@ -1,12 +1,22 @@
-const reposEl = document.querySelector('#repos');
-const usernameEl = document.querySelector('#username');
-
 function loadRepos() {
+	const reposEl = document.querySelector('#repos');
+	const usernameEl = document.querySelector('#username');
+
 	reposEl.innerHTML = '<h1 id="loading">Loading...</h1>';
+
 	const username = usernameEl.value;
 	const url = `https://api.github.com/users/${username}/repos`;
+
 	fetch(url)
-		.then(res => res.json())
+		.then(res => {
+			if (res.status === 404) {
+				let li = document.createElement('li');
+				li.innerHTML = `Username ${username} Not Found`;
+				reposEl.appendChild(li);
+			} else {
+				return res.json()
+			}
+		})
 		.then(data => {
 			document.getElementById('loading').remove();
 			data.forEach(elem => {
